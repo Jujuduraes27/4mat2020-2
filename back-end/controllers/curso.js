16fe0474b9ac212446f96e149022b996be712d6c
Jujuduraes27 <jujubaduraes@hotmail.com>
2020-09-14 14:10:11 +0000

    (14/09 ) Método RETRIEVE

--- back-end/controllers/curso.js
+++ back-end/controllers/curso.js
@@ -1,40 +1,57 @@
 /*
-QUATRO OPERAÇÕES BÁSICAS DE DADOS
-1) CREATE(criação ou inserção)
-criar um novo objeto dentro da coleção.
-2) RETRIEVE (recuperação e listagem)
-permite recuperar os objetos a partir do BD.
-3) UPDATE(atualização)
-Alterar os dados de um objeto que JÁ EXISTE no BD
-4) DELETE(exclusão)
-Elimina um objeto do BD
-
-(C)reate + (R)etrieve + (U)pdate + (D)elete = CRUD
-VERBOS HTTP ASSOCIADOS AS OPERÇÕES CRUD
-
-Verbo  Operação
-POST   Create
-GET    Retrieve
-PUT    Update
-DELETE  Delete
-
+    QUATRO OPERAÇÕES BÁSICAS SOBRE DADOS
+    1) CREATE (criação ou inserção)
+        Cria um novo objeto dentro da coleção
+    2) RETRIEVE (recuperação ou listagem)
+        Permite recuperar os objetos a partir do BD
+    3) UPDATE (atualização)
+        Altera os dados de um objeto que JÁ EXISTE no BD
+    4) DELETE (exclusão)
+        Elimina um objeto do BD
+    (C)reate + (R)etrieve + (U)pdate + (D)elete = CRUD
+    VERBOS HTTP ASSOCIADOS ÀS OPERAÇÕES CRUD
+    Verbo       Operação
+    POST        Create
+    GET         Retrieve
+    PUT         Update
+    DELETE      Delete
 */
-//Importar o model para dentro do controller
+// Importar o model para dentro do controller
 const Curso = require('../models/Curso')
-const controller = {} // objeto vazio
-
-//metodo novo(), implementando a operação CREATE
-controller.novo = async (req,res) =>{
-    try{
-    //Envia os dados dentro de req.body  para o BD para criação
-    await Curso.create(req.body)
-    //HTTP 201: Created
-    res.status(201).end()
+const controller = {}       // Objeto vazio
+// Método novo(), implementando a operação CREATE
+controller.novo = async (req, res) => {
+    try {
+        // Envia os dados dentro de req.body para o BD para criação
+        await Curso.create(req.body)
+        // HTTP 201: Created
+        res.status(201).end()
     }
-    catch(erro){
+    catch(erro) {
         console.error(erro)
-        //HTTP 500: Irternet Server Error
+        // HTTP 500: Internal Server Error
         res.status(500).send(erro)
     }
 }
+// Método listar(), implementando a operação RETRIEVE(all)
+controller.listar = async (req, res) => {
+    try {
+        // find() sem parâmetros é para trazer tudo
+        let dados = await Curso.find()
+        res.send(dados)// Vai com status HTTP 200: OK
+    }
+    catch(erro) {
+        console.error(erro)
+        res.status(500).send(erro)
+    }
+}
+// Metodo obterUm(), implementando a operação   RETRIEVE (ONE)
+controller.obterUm = (req,res) => {
+    const id = req.params.id // Capturando o parametro id
+    let obj = Curso.findById(id)
+    // Se o objeto vier preenchido(achou), então o retornamos  
+    if(obj) res.send(obj)
+    // Senão (objeto vazio), enviamos o status HTTP 404: NOT FOUND
+    else res.status(404).end()
+    }
 module.exports = controller
