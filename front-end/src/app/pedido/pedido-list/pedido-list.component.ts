6e964bad5afea3a7da5bbde4ed3e20644132c8a6
julianaDuraes <juliana.urtado@fatec.sp.gov.br>
2020-12-11 18:37:18 +0000

    trabalho_2_juliana

--- front-end/src/app/pedido/pedido-list/pedido-list.component.ts
+++ front-end/src/app/pedido/pedido-list/pedido-list.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PedidoService } from '../pedido.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-pedido-list',
   templateUrl: './pedido-list.component.html',
   styleUrls: ['./pedido-list.component.scss']
 })
-export class PedidoListComponent implements OnInit {
+export class EntregadorListComponent implements OnInit {
 
-  constructor() { }
+    pedidoes : any = [] // Vetor vazio
+    displayedColumns : string[] = ['pedido','cpf','telefone','editar','excluir']
 
-  ngOnInit(): void {
-  }
+  constructor(private pedidoSrv : PedidoService, 
+  private snackBar: MatSnackBar
+  ) { }
+  
+  async ngOnInit() {
+    this.pedidoes = await this.pedidoSrv.listar() 
+    console.log(this.pedidoes)
+    }
+    async excluir(id: string){
+        if(confirm('Deseja realmente excluir este item?')) {
+            try{
+               // 1) Efetuar a exclusão
+               await this.pedidoSrv.excluir(id)
+               // 2) Atualizar os dados da tabela
+               this.ngOnInit()
+               // 3) dar um feedback para o usuario
+               this.snackBar.open('Item excluido com sucesso', 'Entendi',{
+                   duration:5000 // 5 segundos
+               })
 
+            }
+            catch(erro){
+                // 4) Dar um feedlback de erro para o usuario
+                this.snackBar.open('ERRO: não foi possivel excluir este item.', 'Que pena',{
+                   duration:5000 // 5 segundos
+               })
+            }       
+            
+        }
+    }
 }
